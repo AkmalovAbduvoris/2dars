9d3541e92d82ef3c52f680dc6a142bab78e4fa22
AkmalovAbduvoris <akmalovabduvoris@gmail.com>
2023-12-20 11:09:15 +0500

    Abduvoris

---
+++ js/index.js
@@ -0,0 +1,60 @@
+// function nom1(a , b) {
+// console.log(a + b);
+// }
+// nom1(1 , 3)
+
+
+
+// let nom2 = function(o, b) {
+//     console.log(o + b);
+// }
+// nom2(1, 2)
+
+
+
+
+let nom3 = () => {
+
+}
+
+
+// setTimeout(() => {
+//     nom3(2, 5)
+// }, 5000)
+
+
+
+setInterval(() => {
+    nom3()
+}, 2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
